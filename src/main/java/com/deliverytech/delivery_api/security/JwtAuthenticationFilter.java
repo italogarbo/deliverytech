90b84aaf83bf99b1b1ad5a4a705e6bf542c43324
italogarbo <italo.garbo@gmail.com>
2025-10-24 21:47:21 -0300

    Nova atualização

--- src/main/java/com/deliverytech/delivery_api/security/JwtAuthenticationFilter.java
+++ src/main/java/com/deliverytech/delivery_api/security/JwtAuthenticationFilter.java
@@ -3,6 +3,7 @@ package com.deliverytech.delivery_api.security;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,7 +16,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.lang.NonNull;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
@@ -25,13 +25,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    @Override
+     @Override
     protected void doFilterInternal(
-            @NonNull HttpServletRequest request,
-            @NonNull HttpServletResponse response,
-            @NonNull FilterChain filterChain) throws ServletException, IOException {
+        @NonNull HttpServletRequest request, 
+        @NonNull HttpServletResponse response, 
+        @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
         var token = this.recoverToken(request);
-        if (token != null) {
+        if (token != null){
             var login = jwtUtil.getEmailFromToken(token);
             UserDetails user = usuarioRepository.findByEmail(login);
 
@@ -41,10 +42,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
     }
 
-    private String recoverToken(HttpServletRequest request) {
+    private String recoverToken(HttpServletRequest request){
         var authHeader = request.getHeader("Authorization");
-        if (authHeader == null)
-            return null;
+        if(authHeader == null) return null;
         return authHeader.replace("Bearer ", "");
     }
 }
