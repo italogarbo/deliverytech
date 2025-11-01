a5bbf60311e59835990f95a9d0ef14220d973463
italogarbo <italo.garbo@gmail.com>
2025-10-31 23:01:38 -0300

    Atulização no porm/AuthController/Log/Usuario

---
+++ src/main/java/com/deliverytech/delivery_api/dto/response/UserResponse.java
@@ -0,0 +1,25 @@
+package com.deliverytech.delivery_api.dto.response;
+
+import lombok.Data;
+
+@Data
+
+public class UserResponse {
+
+    private String message;
+    
+    private String email;
+
+    private String nome;
+
+    private String role;
+
+    public static UserResponse fromEntity(Object usuario) {
+
+        UserResponse response = new UserResponse();
+
+        response.setMessage("Usuário cadastrado com sucesso!");
+    
+        return response;
+    }
+}
---
+++ src/main/java/com/deliverytech/delivery_api/service/AuthService.java
@@ -0,0 +1,21 @@
+package com.deliverytech.delivery_api.service;
+
+import com.deliverytech.delivery_api.dto.request.LoginRequest;
+import com.deliverytech.delivery_api.dto.request.RegisterRequest;
+import com.deliverytech.delivery_api.dto.response.LoginResponse;
+import com.deliverytech.delivery_api.dto.response.UserResponse;
+import com.deliverytech.delivery_api.model.Usuario;
+
+public interface AuthService {
+
+    // Cláusula No. 1: Deve saber como processar um login
+    LoginResponse login(LoginRequest request);
+
+    // Cláusula No. 2: Deve saber como registrar um novo usuário
+    Usuario register(RegisterRequest request);
+
+    // Cláusula No. 3: Deve saber como obter informações do usuário logado
+    UserResponse getCurrentUser();
+
+}
+ 
