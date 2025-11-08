2ff8d36811ab7cc092a62c434815aee35d28bea5
italogarbo <italo.garbo@gmail.com>
2025-11-07 22:35:51 -0300

    Atualização necessária para garantir o funcionamento do Docker.

---
+++ src/test/src/test/java/com/deliverytech/delivery_api/DeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deliverytech.delivery_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = DeliveryApiApplication.class)
+class DeliveryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
